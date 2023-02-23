6d6c765d6e9e4707651482db3a169817c2bbbf7a
charliebp1 <clscrea@gmail.com>
2023-02-23 13:53:26 +0100

    first commit

---
+++ src/main/java/com/springboot/webflux/app/controllers/ProductoRestController.java
@@ -0,0 +1,49 @@
+package com.springboot.webflux.app.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.webflux.app.models.documents.Producto;
+import com.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoRestController {
+	
+	@Autowired
+	private ProductoService service;
+	
+	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+	
+	@GetMapping()
+	Flux<Producto> index() {
+		
+		Flux<Producto> productos = service.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).doOnNext((prod -> log.info(prod.getNombre())));
+		
+		return productos;
+	}
+
+	@GetMapping("/{id}")
+	public Mono<Producto> show(@PathVariable String id) {
+		
+		// Mono<Producto> producto = service.findById(id);
+		Flux<Producto> productos = service.findAll();
+		
+		Mono<Producto> producto = productos.filter(p -> p.getId().equals(id))
+				.next()
+				.doOnNext((prod -> log.info(prod.getNombre())));
+		
+		return producto;
+	}
+}
